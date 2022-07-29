d1f5c2ef5115c8c533753551b37dbcf152bdd8a4
timrooke1991 <timrooke1991@gmail.com>
2022-07-29 15:17:16 +0100

    basic shopping list

---
+++ shopping_list/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+import uuid
+
+
+class ShoppingItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    purchased = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.name}"
